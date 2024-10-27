1924557598ba17ec275e4237a03aa728a2fa38cb
24aeronyx <11211017@student.itk.ac.id>
2024-10-27 17:45:39 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      tes
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Menyertakan seluruh file JS/JSX di folder src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
